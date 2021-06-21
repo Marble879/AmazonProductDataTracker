262781fca50f69330bf472b3f2b497c0d2111f9a
Marble879 <younisakel879@gmail.com>
2021-06-21 15:54:49 +0200

    #7 Add setupNotification menu

--- src/main/java/userinterface/MainMenu.java
+++ src/main/java/userinterface/MainMenu.java
@@ -4,19 +4,23 @@ import utils.IoUtils;
 public class MainMenu {
 
     public void startMenu() {
-        System.out.println("Please select an option: "+ IoUtils.EOL + "1. View tracked items" + IoUtils.EOL + "2. Add a new item to be tracked" + IoUtils.EOL);
+        System.out.println("Please select an option: "+ IoUtils.EOL + "1. View tracked items" + IoUtils.EOL + "2. Add a new item to be tracked" + IoUtils.EOL + "3. exit" + IoUtils.EOL);
     }
 
     public void addUrlMenu(){
-        System.out.println("Please input the URL: ");
+        System.out.println("Please select an option: " + IoUtils.EOL + "1. Add a new URL" + "2. return" + IoUtils.EOL);
     }
 
     public void setupNotificationsMenu(){
-        System.out.println();
+        System.out.println("Please select an option: " + IoUtils.EOL + "1. Enable notifications" + IoUtils.EOL + "2. Disable notifications" + IoUtils.EOL + "3. return" + IoUtils.EOL);
     }
 
-    private void addEmailAddressMenu(){
+    public void addNewURLMessage(){ // will be used by the addUrlMenu()
+        System.out.println("Please input the URL: " + IoUtils.EOL);
+    }
 
+    public void addEmailAddressMessage() { // Will be used by sssetupNotiicationsMenu()
+        System.out.println("Please input your email: " + IoUtils.EOL);
     }
 
 }
