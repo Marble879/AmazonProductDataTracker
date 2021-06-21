7465f9c8718663c6256991f5e8c526a255f8d946
Marble879 <younisakel879@gmail.com>
2021-06-21 13:34:43 +0200

    #7 Add start and url menu

--- src/main/java/userinterface/MainMenu.java
+++ src/main/java/userinterface/MainMenu.java
@@ -1,2 +1,22 @@
-package userinterface;public class MainMenu {
+package userinterface;
+import utils.IoUtils;
+
+public class MainMenu {
+
+    public void startMenu() {
+        System.out.println("Please select an option: "+ IoUtils.EOL + "1. View tracked items" + IoUtils.EOL + "2. Add a new item to be tracked" + IoUtils.EOL);
+    }
+
+    public void addUrlMenu(){
+        System.out.println("Please input the URL: ");
+    }
+
+    public void setupNotifications(){
+        System.out.println();
+    }
+
+    private void addEmailAddress(){
+
+    }
+
 }
