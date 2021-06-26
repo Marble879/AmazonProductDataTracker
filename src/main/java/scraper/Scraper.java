4c287adf23cc8079232b441c42c5d2de13f71702
Marble879 <younisakel879@gmail.com>
2021-06-26 22:12:09 +0200

    #5 Add methods to get discount information on the tracked product

--- src/main/java/scraper/Scraper.java
+++ src/main/java/scraper/Scraper.java
@@ -2,7 +2,9 @@ package scraper;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import utils.IoUtils;
 
 public class Scraper {
 
@@ -27,9 +29,27 @@ public class Scraper {
 
     public String getProductPrice(){
         Elements elemPrice = doc.select("#priceblock_ourprice");
+        String discount;
         String stringPrice = elemPrice.html().replace("&nbsp;", "");
-
+        if(isDiscount()){
+            discount = receiveDiscount();
+            stringPrice = stringPrice + IoUtils.EOL + "Discount is: " + discount + IoUtils.EOL;
+        }
         return stringPrice;
     }
+
+    public boolean isDiscount(){ // checks if there is a discount.
+        Elements elemDiscount = doc.select("#regularprice_savings");
+        return elemDiscount.size() > 0;
+    }
+
+    public String receiveDiscount(){
+        Elements elemDiscount = doc.select("#regularprice_savings");
+        String stringPrice = elemDiscount.html().replace("&nbsp;", "");
+        return stringPrice;
+    }
+
+    //TODO check if the returned price from getProductPrice is discounted. If it is, make sure to check this, and then output to user this is price on sale.
+    //TODO if discounted is the returned one, return % of discount, return before price and mention that there is a discount!
 }
 
