58af1a9431bc0bb9d89e8cef5bbc0a1ba020bcf0
Marble879 <younisakel879@gmail.com>
2021-07-22 00:05:32 +0200

    #12 Add util for output presentation

--- src/main/java/utils/IoUtils.java
+++ src/main/java/utils/IoUtils.java
@@ -3,4 +3,6 @@ package utils;
 public class IoUtils {
 
     public static final String EOL = System.lineSeparator();
+
+    public static final String OUTPUT_LINE = "------------------------------";
 }
